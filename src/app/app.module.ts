b0568504fbec26509551f37d7d2886f253249b1a
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-05-04 23:37:42 -0500

    Hemos instalado firebase y angular fire.. y en el app.module.ts importamos afAuthModule,afDatabaseModule, el enviroment, y por supuesto AngularFireModule.. en los imports importamos todo lo llamado de la misma formahabitual exceptuando AngularFireModile y enviroment, estos iràn asì: AngularFireModule.initializeApp(enviroment.firebsseConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// ANGULAR FIRE
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
