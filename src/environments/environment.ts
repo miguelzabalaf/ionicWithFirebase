6e82bd32ec039e46ce5a30cf0765228186a86b03
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-05-04 23:05:04 -0500

    He incrustado el objeto en enviroments que hace la conexión a firebase, este mismo código lo debo colocar en enviroments.prod cuando vaya a producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBVwnygQP07QRhLoQ-00PurawBynCFsukw",
+    authDomain: "ionicwithfirebase-c0f51.firebaseapp.com",
+    databaseURL: "https://ionicwithfirebase-c0f51.firebaseio.com",
+    projectId: "ionicwithfirebase-c0f51",
+    storageBucket: "ionicwithfirebase-c0f51.appspot.com",
+    messagingSenderId: "999193160922",
+    appId: "1:999193160922:web:c62fda446251bef62ec9e3"
+  }
 };
 
 /*
