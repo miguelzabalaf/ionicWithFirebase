1a2e9f0c705d236081c2cc6cd874ea7698672f4e
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-05-05 00:14:40 -0500

    He podido llamar la data suminstrada en mi base de datos de firebase llamada test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { environment } from 'src/environments/environment';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -12,7 +13,6 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  // DECLARAMOS LA PROPIEDAD ITEMREF
+  itemRef: any;
+  // INSTANCIAMOS EL ANGULARFIREDATABASE
+  constructor( private db: AngularFireDatabase ) {}
 
+  ngOnInit() {
+    this.itemRef = this.db.object('test');
+    this.itemRef.snapshotChanges().subscribe(action => {
+    console.log(action.type);
+    console.log(action.key)
+    console.log(action.payload.val())
+    });
+  }
 }
